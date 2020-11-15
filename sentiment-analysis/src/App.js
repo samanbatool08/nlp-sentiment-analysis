3dd784a7a892c73d9ac8adf06ac2a7d7e8d590a5
Your Name <samanbatool08@gmail.com>
2020-11-15 17:37:54 -0500

    setup state for sentiment analysis

--- sentiment-analysis/src/App.js
+++ sentiment-analysis/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sentiment from 'sentiment';
+
+const sentiment = new Sentiment();
 
 
 class App extends Component {
@@ -7,8 +10,9 @@ class App extends Component {
   constructor(props){
   super(props);
   this.state = {
-
-  }
+    sentimentScore: null,
+    generalSentiment: null;
+  };
 }
 
 render() {
@@ -16,6 +20,7 @@ render() {
     <div className='App'>
       <h2>Text Sentiment Analysis</h2>
       <p>Enter for real-time analysis:</p>
+      
     </div>
   )
 }
