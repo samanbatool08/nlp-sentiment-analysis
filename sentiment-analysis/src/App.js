cb761bc74c301ce321797ae874261958b21d3815
Your Name <samanbatool08@gmail.com>
2020-11-15 17:22:43 -0500

    change to class component

--- sentiment-analysis/src/App.js
+++ sentiment-analysis/src/App.js
@@ -2,5 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 
+class App extends Component {
+
+  constructor(props){
+  super(props);
+  this.state = {
+
+  }
+}
+
+render() {
+  return (
+    <div>
+    </div>
+  )
+}
+}
+
 
 export default App;
